036e8d2ef4292d409da0ede04464dccec55c23fe
Shahakbk <shahakbk@gmail.com>
2017-06-29 10:55:59 +0300

    Initial commit

---
+++ Wet/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Shahak" << std::endl;
+    return 0;
+}
---
+++ Wet/tests/EnigmaTest.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Shahak on 29/06/2017.
+//
+
+#include <iostream>
+
+#include "../Enigma.h"
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+
+
+    return 0;
+}
