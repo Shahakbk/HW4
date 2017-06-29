2373aa1e1c3d2f27a6e1564cbd23f341c1f39341
adirez <adireznik@campus.technion.ac.il>
2017-06-29 15:14:05 +0300

    working on wrapper, exceptions and stream left

--- Wet/EscapeRoomWrapper.cpp
+++ Wet/EscapeRoomWrapper.cpp
@@ -15,16 +15,63 @@ EscapeRoomWrapper::EscapeRoomWrapper(char *name, const int &escapeTime,
 }
 
 EscapeRoomWrapper::EscapeRoomWrapper(char *name, const int &level) {
-
-    escape_room = escapeRoomCreate(name, 60, level, 0);
+    escape_room = escapeRoomCreate(name, 60, level, 6);
 }
 
-EscapeRoomWrapper::EscapeRoomWrapper(const EscapeRoomWrapper &room) {
-
-    escape_room = escapeRoomCopy(room.escape_room);
+EscapeRoomWrapper::EscapeRoomWrapper(const EscapeRoomWrapper &room) :
+        escape_room(escapeRoomCopy(room.escape_room)) {
 }
 
 EscapeRoomWrapper::~EscapeRoomWrapper() {
-
     escapeRoomDestroy(escape_room);
+}
+
+EscapeRoomWrapper& EscapeRoomWrapper::operator=(const EscapeRoomWrapper &room) {
+    if(this == &room){
+        return *this;
+    }
+    escapeRoomDestroy(escape_room);
+    escape_room = escapeRoomCopy(room.escape_room);
+    return *this;
+}
+
+
+bool EscapeRoomWrapper::operator==(const EscapeRoomWrapper &room) const {
+    return areEqualRooms(escape_room, room.escape_room);
+}
+
+bool EscapeRoomWrapper::operator!=(const EscapeRoomWrapper &room) const {
+    return !areEqualRooms(escape_room, room.escape_room);
+}
+
+bool EscapeRoomWrapper::operator<(const EscapeRoomWrapper &room) const {
+    return !isBiggerRoom(escape_room, room.escape_room);
+}
+
+bool EscapeRoomWrapper::operator>(const EscapeRoomWrapper &room) const {
+    return isBiggerRoom(escape_room, room.escape_room);
+}
+
+int EscapeRoomWrapper::level() const {
+    return getLevel(escape_room);
+}
+
+void EscapeRoomWrapper::rate(const int &newRate) {
+    updateRate(escape_room, newRate);
+}
+
+std::string EscapeRoomWrapper::getName() const {
+    return roomGetName(escape_room);
+}
+
+double EscapeRoomWrapper::getRate() const {
+    return roomGetRate(escape_room);
+}
+
+int EscapeRoomWrapper::getMaxTime() const {
+    return roomGetMaxTime(escape_room);
+}
+
+int EscapeRoomWrapper::getMaxParticipants() const {
+    return roomGetMaxParticipants(escape_room);
 }
