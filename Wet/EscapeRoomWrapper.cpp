6cc5b5e7c57f079e2e7cfbb1bde87efa7eb324bc
adirez <adireznik@campus.technion.ac.il>
2017-06-30 18:03:06 +0300

    finished wrapper

--- Wet/EscapeRoomWrapper.cpp
+++ Wet/EscapeRoomWrapper.cpp
@@ -3,38 +3,40 @@
 //
 
 #include "EscapeRoomWrapper.h"
+#include "Exceptions.h"
 
 using namespace mtm::escaperoom;
 using std::string;
 
 EscapeRoomWrapper::EscapeRoomWrapper(char *name, const int &escapeTime,
                                      const int &level,
-                                     const int &maxParticipants) {
-
-    escape_room = escapeRoomCreate(name, escapeTime, level, maxParticipants);
+                                     const int &maxParticipants) :
+        escape_room(escapeRoomCreate(name, escapeTime, level, maxParticipants)){
+    if (escape_room == NULL) {
+        throw EscapeRoomMemoryProblemException();
+    }
 }
 
-EscapeRoomWrapper::EscapeRoomWrapper(char *name, const int &level) {
-    escape_room = escapeRoomCreate(name, 60, level, 6);
+EscapeRoomWrapper::EscapeRoomWrapper(char *name, const int &level) :
+        escape_room(escapeRoomCreate(name, 60, level, 6)) {
+    if (escape_room == NULL) {
+        throw EscapeRoomMemoryProblemException();
+    }
 }
 
 EscapeRoomWrapper::EscapeRoomWrapper(const EscapeRoomWrapper &room) :
         escape_room(escapeRoomCopy(room.escape_room)) {
-}
-
-EscapeRoomWrapper::~EscapeRoomWrapper() {
-    escapeRoomDestroy(escape_room);
-}
-
-EscapeRoomWrapper& EscapeRoomWrapper::operator=(const EscapeRoomWrapper &room) {
-    if(this == &room){
-        return *this;
+    if (escape_room == NULL) {
+        throw EscapeRoomMemoryProblemException();
     }
+}
+
+EscapeRoomWrapper &EscapeRoomWrapper::operator=(const EscapeRoomWrapper &room) {
+    EscapeRoom tmp = escapeRoomCopy(room.escape_room);
     escapeRoomDestroy(escape_room);
-    escape_room = escapeRoomCopy(room.escape_room);
+    escape_room = tmp;
     return *this;
 }
-
 
 bool EscapeRoomWrapper::operator==(const EscapeRoomWrapper &room) const {
     return areEqualRooms(escape_room, room.escape_room);
@@ -57,7 +59,19 @@ int EscapeRoomWrapper::level() const {
 }
 
 void EscapeRoomWrapper::rate(const int &newRate) {
+    if (newRate < 0 || newRate >5) {
+        throw EscapeRoomIllegalRateException();
+    }
     updateRate(escape_room, newRate);
+}
+
+EscapeRoomWrapper::~EscapeRoomWrapper() {
+    escapeRoomDestroy(escape_room);
+}
+
+std::ostream& operator<<(std::ostream& output, const EscapeRoomWrapper& room){
+    return output << getName() << "(" << getMaxTime() << "/" << level() <<
+                  "/" << getMaxParticipants() << ")";
 }
 
 std::string EscapeRoomWrapper::getName() const {
